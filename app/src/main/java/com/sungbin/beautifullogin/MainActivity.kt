a227582308a993c9e2a4dec2314005bc3514b18e
SungBin5304 <sungbin.dev@gmail.com>
2020-05-23 18:49:46 +0900

    Initial commit

---
+++ app/src/main/java/com/sungbin/beautifullogin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sungbin.beautifullogin
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+}
